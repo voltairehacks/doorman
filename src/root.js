26c2bc95d6e9a7d178884d3d6e59ccf2119975ba
Esteban Ordano <eordano@gmail.com>
2016-04-02 16:20:15 -0300

    Add facebook-login

--- src/root.js
+++ src/root.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 import AssociateForm from './associate/form'
 import CurrentlyOnline from './current/list'
+import FbConnect from './facebook'
 
 export default class Root extends React.Component {
   render() {
     return <div>
       <h1>Voltaire</h1>
       <CurrentlyOnline />
-      <h2>Register</h2>
-      <AssociateForm />
+      <FbConnect />
     </div>
   }
 }
